5d7cd18386e8ddb2a12aea550277529fc031fff4
DaniyalHusssain <danhussain29@gmail.com>
2024-04-12 22:42:51 +0500

    added the api of image generator

--- controller/openAi/openAi.js
+++ controller/openAi/openAi.js
@@ -1,21 +1,29 @@
 const OpenAi = require("openai");
 const ImageModel = require("../../models/ImageSchem");
+require("dotenv").config();
 
+console.log(process.env.apidalle);
 const openai = new OpenAi({
-  apiKey: "api",
+  apiKey: process.env.apidalle,
 });
 
 module.exports.textToImageGen = async (req, res) => {
   const { prompt } = req.body;
 
   try {
-    response = await openai.images.generate({
+    const response = await openai.images.generate({
       prompt: prompt,
       n: 1,
       size: "256x256",
     });
 
-    const imageUrl = response.images[0].url;
+    // Check if the response contains the expected data structure
+    if (!response.data || response.data.length === 0 || !response.data[0].url) {
+      throw new Error("No images generated or response format incorrect");
+    }
+
+    // Extract the image URL
+    const imageUrl = response.data[0].url;
 
     const newImage = new ImageModel({
       imageUrl: imageUrl,
@@ -26,6 +34,7 @@ module.exports.textToImageGen = async (req, res) => {
 
     res.status(200).json({ message: imageUrl });
   } catch (error) {
-    res.status(500).json({ message: error });
+    console.error(error); // Log the error for further investigation
+    res.status(500).json({ message: error.message });
   }
 };
